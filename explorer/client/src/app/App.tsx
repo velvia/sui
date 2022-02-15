32ffa7eb13c76ef6f86b55cd765885ea4bead0f8
Andrew Burnie <apburnie@hotmail.co.uk>
2022-02-15 16:08:27 +0000

    creates basic transaction results page

--- explorer/client/src/app/App.tsx
+++ explorer/client/src/app/App.tsx
@@ -1,7 +1,75 @@
+import React from 'react';
+
+import mockTransactionData from '../utils/transaction_mock.json'
 import appStyles from './App.module.scss';
 
 function App() {
-    return <div className={appStyles.app}>No transactions here yet</div>;
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('txn_id');
+
+  const data = mockTransactionData.data.find(
+        ({id}) => id === query
+      )
+
+  let action; 
+  let objectID;
+    
+  if (data) {
+    if ("created" in data) {
+      action = "Create"
+      objectID = data?.created?.[0]
+    } else if ("deleted" in data) {
+      action = "Delete"
+      objectID = data?.deleted?.[0]
+    } else
+    if ("mutated" in data) {
+      action = "Mutate"
+      objectID = data?.mutated?.[0]
+    } else 
+    {
+      action = "Unknown"
+      objectID = "Unknown"
+    }
+    } else {
+      action = "No Data"
+      objectID = "No Data"
+    }
+
+  console.log(mockTransactionData)
+    return <div className={appStyles.app}>
+      <h1>Mysten Labs</h1> 
+      <h2>The Sui Explorer</h2> 
+    <form action="/" method="get">
+      <input
+        type="text"
+        id="search"
+        placeholder="Search transactions by ID" 
+        name="txn_id"/>
+      <button type="submit">Search</button>
+    </form>
+    {(query) ? (
+      <div>
+        <br/>
+        <div>Transaction ID</div> 
+        <div>{data?.id}</div>
+        <br/>
+        <div>Status</div> 
+        <div>{data?.status}</div> 
+        <br/>
+        <div>Sender</div> 
+        <div>{data?.sender}</div>
+        <br/>
+        <div>Did</div> 
+        <div>{action}</div>
+        <br/>
+        <div>Object</div> 
+        <div>{objectID}</div>
+      </div> 
+    ) : (
+      <div>No Latest Transactions</div>
+    )
+    }
+    </div>;
 }
 
 export default App;
