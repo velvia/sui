9d5c69aa065d404e88f87840042aa8c61b1484de
Pavlos Chrysochoidis <10210143+pchrysochoidis@users.noreply.github.com>
2022-02-16 13:53:21 +0000

    explorer-client: use react-bootstrap
    * added a demo modal

--- explorer/client/src/app/App.tsx
+++ explorer/client/src/app/App.tsx
@@ -1,7 +1,51 @@
+import cn from 'classnames';
+import { useCallback, useState } from 'react';
+import { Container, Row, Col, Navbar, Button, Modal } from 'react-bootstrap';
+
 import appStyles from './App.module.scss';
 
 function App() {
-    return <div className={appStyles.app}>No transactions here yet</div>;
+    const [modalVisible, setModalVisible] = useState(false);
+    const onHandleShowModal = useCallback(() => {
+        setModalVisible(true);
+    }, [setModalVisible]);
+    const handleHideModal = useCallback(() => {
+        setModalVisible(false);
+    }, [setModalVisible]);
+    return (
+        <>
+            <Navbar expand={true} variant="dark" bg="dark">
+                <Container>
+                    <Navbar.Brand>Sui Explorer</Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Container className={cn(appStyles.app, 'mt-1')}>
+                <Row>
+                    <Col>No transactions here yet</Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button variant="secondary" onClick={onHandleShowModal}>
+                            Open Modal
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+            <Modal show={modalVisible} onHide={handleHideModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Hello</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>This is a demo</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleHideModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
 export default App;
