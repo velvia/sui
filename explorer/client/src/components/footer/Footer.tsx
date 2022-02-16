6cc2c2aeaf773894a58423c35f362710f7d6c11b
Andrew Burnie <apburnie@hotmail.co.uk>
2022-02-16 19:22:29 +0000

    further changes to footer

--- explorer/client/src/components/footer/Footer.tsx
+++ explorer/client/src/components/footer/Footer.tsx
@@ -1,9 +1,15 @@
 import styles from './Footer.module.scss'
 
 function Footer() {
-    return <footer className={styles.footer}>
-      <a href="/">Home</a>
-    </footer>;
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.links}>
+          <a href="/">Home</a>
+          <a href="https://mystenlabs.com/">Mysten Labs</a>
+          <a href="https://devportal-30dd0.web.app/">Developer Hub</a>
+        </div>
+      </footer>
+    );
 }
 
 export default Footer;
